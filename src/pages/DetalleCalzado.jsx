b0d7d07e7d3ca83c9e8aed602a215f10df88d2d5
Brunno <rivasbrunno10@gmail.com>
2025-10-07 19:18:47 -0300

    boton volver atras

--- src/pages/DetalleCalzado.jsx
+++ src/pages/DetalleCalzado.jsx
@@ -1,12 +1,15 @@
 import { Container, Card } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import calzados from '../data/calzados';
 import Imagen from '../components/atoms/Imagen.jsx';
 import Texto from '../components/atoms/Texto.jsx';
+import Boton from '../components/atoms/Boton.jsx';
+import { useNavigate } from 'react-router-dom';
 
 
 function DetalleCalzado() {
  const { id } = useParams();
+ const navigate = useNavigate();
  const calzado = calzados.find((p) => p.id === parseInt(id));
 
 
@@ -21,7 +24,9 @@ function DetalleCalzado() {
 
  return (
    <Container className="my-5">
+     
      <Card>
+      <Boton variant="primary" onClick={() => navigate('/catalogo')}>Volver atras</Boton>
       <Imagen src={calzado.imagen} alt={calzado.titulo} className="card-img-top" />
        <Card.Body>
          <Texto variant="h2">{calzado.titulo}</Texto>
