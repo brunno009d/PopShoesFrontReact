6c27aa1cc4a8f01ca9c8df863d888625b9992794
Brunno <rivasbrunno10@gmail.com>
2025-10-08 13:26:11 -0300

    boton comprar

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,6 +1,7 @@
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import { useCart } from '../context/CarritoContext';
 import Imagen from '../components/atoms/Imagen';
+import Boton from '../components/atoms/Boton';
 
 function Carrito() {
   const { carrito } = useCart();
@@ -10,6 +11,7 @@ function Carrito() {
       <Container className="my-5">
         <h1>Carrito de Compras</h1>
         <p>Tu carrito está vacío</p>
+        <Boton type="button" class="btn btn-lg btn-primary" disabled>Comprar</Boton>
       </Container>
     );
   }
@@ -39,6 +41,7 @@ function Carrito() {
           </Col>
         ))}
       </Row>
+      <Boton type="button" class="btn btn-lg btn-primary">Comprar</Boton>
     </Container>
   );
 }
