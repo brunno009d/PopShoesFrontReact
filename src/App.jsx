47b837abc0c470a5326696090d6649e1a3165f98
MauriLS <mauriciolillo122@gmail.com>
2025-10-08 12:41:09 -0300

    navbar act 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/organisms/Navbar';
-import Home from './pages/Home';
-import Catalogo from './pages/Catalogo';
-import DetalleCalzado from './pages/DetalleCalzado';
-
-
+import { Routes, Route } from "react-router-dom";
+import AppNavbar from "./components/organisms/Navbar";
+import Home from "./pages/Home";
+import Catalogo from "./pages/Catalogo";
+import DetalleCalzado from "./pages/DetalleCalzado";
 
 function App() {
- return (
-   <>
-     <NavBar />
-     <Routes>
-       <Route path="/" element={<Home />} />
-       <Route path="/catalogo" element={<Catalogo />} />   
-       <Route path="/calzados/:id" element={<DetalleCalzado />} />    
-     </Routes>
-   </>
- );
+  return (
+    <>
+      <AppNavbar />
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalogo" element={<Catalogo />} />
+        <Route path="/calzados/:id" element={<DetalleCalzado />} />
+      </Routes>
+    </>
+  );
 }
-
 
 export default App;
--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -1,23 +1,62 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
+function AppNavbar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+      <Container>
+        {/* Logo */}
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src="/assets/img/logo.png"
+            alt="PopShoes"
+            height="40"
+            className="d-inline-block align-top me-2"
+          />
+          PopShoes
+        </Navbar.Brand>
 
+        {/* Toggle para móvil */}
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          {/* Menú principal */}
+          <Nav className="me-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/catalogo/marcas">
+              <Nav.Link>Marcas</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/catalogo/hombre">
+              <Nav.Link>Hombre</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/catalogo/mujer">
+              <Nav.Link>Mujer</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/catalogo/estilos">
+              <Nav.Link>Estilos</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/catalogo/famosos">
+              <Nav.Link>Famosos</Nav.Link>
+            </LinkContainer>
+          </Nav>
 
-function NavBar() {
- return (
-   <Navbar bg="dark" variant="dark" expand="lg">
-     <Container>
-       <Navbar.Brand href="/">Ruteando</Navbar.Brand>
-       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-       <Navbar.Collapse id="basic-navbar-nav">
-         <Nav className="me-auto">
-           <Nav.Link href="/">Inicio</Nav.Link>
-           <Nav.Link href="/catalogo">Catalogo</Nav.Link>
-         </Nav>
-       </Navbar.Collapse>
-     </Container>
-   </Navbar>
- );
+          {/* Acciones (carrito y login) */}
+          <Nav>
+            <LinkContainer to="/carrito">
+              <Nav.Link>
+                <i className="fa-solid fa-cart-shopping"></i>
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Inicio Sesión</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-
-export default NavBar;
+export default AppNavbar;
