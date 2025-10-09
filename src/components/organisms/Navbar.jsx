0c6be69b5ea1815cf06bd23ea572fc04126ac998
MauriLS <mauriciolillo122@gmail.com>
2025-10-09 17:00:04 -0300

    avances en los estilos de todo el sitio

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -1,26 +1,64 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button, Image } from 'react-bootstrap';
 import { useCart } from '../../context/CarritoContext';
-
+import { useNavigate } from 'react-router-dom';
+import { Cart4 } from 'react-bootstrap-icons';
+import '../../styles/Navbar.css'; 
 
 function NavBar() {
   const { actualizarNumeroCarrito } = useCart();
+  const navigate = useNavigate();
 
- return (
-   <Navbar bg="dark" variant="dark" expand="lg">
-     <Container>
-       <Navbar.Brand href="/">Ruteando</Navbar.Brand>
-       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-       <Navbar.Collapse id="basic-navbar-nav">
-         <Nav className="me-auto">
-           <Nav.Link href="/">Inicio</Nav.Link>
-           <Nav.Link href="/catalogo">Catalogo</Nav.Link>
-           <Nav.Link href="/carrito">Carrito {actualizarNumeroCarrito}</Nav.Link>
-         </Nav>
-       </Navbar.Collapse>
-     </Container>
-   </Navbar>
- );
+  return (
+    <Navbar className="custom-navbar shadow-sm py-2" variant="dark" expand="lg" sticky="top" >
+      <Container className="d-flex align-items-center justify-content-between">
+        {/* Logo a la izquierda */}
+        <div
+          className="d-flex align-items-center"
+          style={{ cursor: 'pointer' }}
+          onClick={() => navigate('/')}
+        >
+          <Image
+            src="./imghome/logo.webp" 
+            alt="Logo PopShoes"
+            height={100}
+            className="me-2"
+          />
+        </div>
+
+        {/* Botón hamburguesa en móvil */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        {/* Contenido colapsable */}
+        <Navbar.Collapse id="basic-navbar-nav ">
+          {/* Enlaces centrados */}
+          <Nav className="mx-auto text-center">
+            <Nav.Link onClick={() => navigate('/')}>Inicio</Nav.Link>
+            <Nav.Link onClick={() => navigate('/catalogo')}>Catálogo</Nav.Link>
+          </Nav>
+
+          {/* Carrito a la derecha */}
+          <div className="d-flex justify-content-end">
+            <Button
+              variant="outline-light"
+              className="d-flex align-items-center position-relative"
+              onClick={() => navigate('/carrito')}
+            >
+              <Cart4 className="me-2" size={18} />
+              Carrito
+              {actualizarNumeroCarrito > 0 && (
+                <span
+                  className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+                  style={{ fontSize: '0.75rem' }}
+                >
+                  {actualizarNumeroCarrito}
+                </span>
+              )}
+            </Button>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-
 
 export default NavBar;
